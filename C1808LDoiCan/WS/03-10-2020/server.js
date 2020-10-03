2fa4151ebe6efaf60d813b1e2659227bb9c225fa
Nguyen Duc Hoang <nguyenduchoang@Nguyens-MacBook-Pro.local>
2020-10-03 21:34:44 +0700

    update

--- C1808LDoiCan/WS/03-10-2020/server.js
+++ C1808LDoiCan/WS/03-10-2020/server.js
@@ -1,7 +1,7 @@
 //Cai cac thu vien
 //npm install body-parser express express-fileupload 
 const express = require('express')
-console.log(`express = ${express}`)
+debugger
 const app = express()
 const MAXMUM_FILE_SIZE = 50
 const bodyParser = require('body-parser')
@@ -12,6 +12,9 @@ app.use(bodyParser.json())
 app.use(fileUpload({
     limits: { fileSize: MAXMUM_FILE_SIZE * 1024 * 1024 },  //Maximum = 50 MB  
 }))
+//viet cac router
+const CustomerRoute = require('./routings/customer')
+app.use('/customers', CustomerRoute)
 PORT = 3000
 app.listen(PORT, () => {
     console.log(`app listen from : ${PORT}`)    
