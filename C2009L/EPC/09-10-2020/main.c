3c33509927d8b6e2d1e0a92cb0b05bdcd0400d76
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-10-09 21:14:00 +0700

    update

--- C2009L/EPC/09-10-2020/main.c
+++ C2009L/EPC/09-10-2020/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> 
+
 //stdio = standard input / output
 //standard library, h = header
 //int = integer = so nguyen
@@ -27,7 +29,39 @@ void chepPhat() {
 	}
 	printf("Chep phat xong");
 }
+void solve() {
+	double a,b,c;
+	double x1,x2,x;
+	double delta;
+	printf("Enter a = "); scanf("%lf", &a); 
+	//lf = long float = "double"
+	printf("Enter b = "); scanf("%lf", &b);
+	printf("Enter c = "); scanf("%lf", &c);
+	if(a == 0.0) {
+		printf("You must enter a > 0");
+		return;
+	}
+	delta = b * b - 4 * a * c;	
+	printf("haha = %f", delta);
+	if(delta > 0){
+		printf("Hai nghiem phan biet \n");
+		//sqrt = squared root
+		x1 = (-b + sqrt(delta)) / (2 * a);
+		x2 = (-b - sqrt(delta)) / (2 * a);
+		printf("x1 = %0.2f \n", x1);
+		printf("x2 = %0.2f \n", x2);
+	} else if(delta == 0) {
+		printf("One result \n");
+		x = -b / (2 * a);
+		printf("x=%0.2fabcd \n", x);
+	} else {
+		printf("No result \n");
+	}
+}
+
 int main(int argc, char *argv[]) {
+	solve();
+	return 0;	
 	chepPhat();
 	//Goi ham tinh tong hai so 
 	printf("Chuong trinh tinh tong 2 so\n");//\n = returN
@@ -40,11 +74,5 @@ int main(int argc, char *argv[]) {
 	x = x + 1;
 	printf("Gia tri cua x = %d",x);
 	checkEvenNumber();
-	return 0;
-	//Ham main, neu thanh cong thi return 0, nguoc lai thi khac 0
-	/*
-	Day la 1 chuong trinh don gian
-	tinh tong hai  so
-	*/
-	
+	return 0;		
 } 
