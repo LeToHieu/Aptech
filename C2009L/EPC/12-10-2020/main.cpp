25395ea87f79b11df8e1c6cda96ad2087a38dd7e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-10-14 16:40:06 +0700

    update

--- C2009L/EPC/12-10-2020/main.cpp
+++ C2009L/EPC/12-10-2020/main.cpp
@@ -13,9 +13,38 @@ float getArea(float radius) {
 	return pow(radius, 2) * M_PI; //pow = power
 }
 float convertCelciusToFarenheit(float celcius)  {
+	//convert_celcius_to_farenheit => c, python, ruby, php
+	//convertCelciusToFarenheit => c, java, c#, Swift,..
+	//convert_CelciusTo_Fa => don't do this !
 	return celcius * (9.00/5.00) + 32.0;
 }
+void calculateMarks() {
+	double math, physics, chemistry, biology;
+	double sum, average;
+	printf("Enter math : "); scanf("%lf", &math);
+	printf("Enter physics : "); scanf("%lf", &physics);
+	printf("Enter chemistry : "); scanf("%lf", &chemistry);
+	printf("Enter biology : "); scanf("%lf", &biology);
+	sum = math + physics + chemistry + biology;
+	average = sum / 4.00;
+	printf("sum = %0.2lf, average = %0.2lf\n", sum, average);
+}
+void doSomething() {
+	float x;//0.0
+	printf("value of x = %f\n", x);
+	printf("address of x = %p\n", &x);
+	//khac nhau giua string va char(character)
+	char choice = 'y';//this is a char
+	//char name[200];//string
+	string myname = "Hoang";
+	cout<<"Your name is :"<<myname<<endl;
+
+}
+
 int main(int argc, char *argv[]) {
+	doSomething();
+	return 0;
+	calculateMarks();	
 	float celcius, farenheit;
 	cout<<"Enter celcius :"<<endl;
 	cin>>celcius;
