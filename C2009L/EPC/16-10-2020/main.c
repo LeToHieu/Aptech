01cbacaecb76662c2a482e4b7311fdebaf8efeb7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-10-16 19:10:24 +0700

    update

---
+++ C2009L/EPC/16-10-2020/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <conio.h>
+int x, y;
+
+char getInputOneCharacter() {
+	char choice;
+	choice = getchar();
+	while(choice == '\n') {
+		choice = getchar();		
+	}
+	
+	return choice;
+}
+void sum() {			
+	printf("x = "); scanf("%d", &x);
+	printf("y = "); scanf("%d", &y);
+	printf("Tong = %d", x + y);
+}
+void minus() {			
+	printf("x = "); scanf("%d", &x);
+	printf("y = "); scanf("%d", &y);
+	printf("Hieu = %d", x - y);
+}
+int main(){	
+	//chua noi gi den tinh toan, hay lam menu da
+	char choice;	
+	while(choice != 'c' && choice != 'C') {
+		printf("1.Tinh tong\n");
+		printf("2.Tinh hieu\n");			
+		printf("3.Ket thuc\n");			
+		printf("Moi ban chon 1,2,3:\n");
+		choice = getInputOneCharacter();
+		switch(choice) {
+			case '1':
+				printf("1.Tinh tong\n");
+				sum();
+				break;
+			case '2':
+				printf("2.Tinh hieu\n");	
+				minus();		
+				break;
+			case '3':
+				printf("3.Ket thuc\n");			
+				break;
+			default:
+				break;
+		}
+		printf("Ban co muon ket thuc(c,k) ?\n");
+		choice = getInputOneCharacter();
+		printf("haha");
+	}
+	printf("Chuong trinh da ket thuc\n");
+	return 0;	
+	
+}
+
