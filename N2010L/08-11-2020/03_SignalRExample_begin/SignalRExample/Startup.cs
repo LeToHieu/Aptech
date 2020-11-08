18a75fa0824b7ab7b296532862464c78deb6b8e7
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-11-08 11:19:51 +0700

    update

---
+++ N2010L/08-11-2020/03_SignalRExample_begin/SignalRExample/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using SignalRExample.Hubs;
+using SignalRExample.Middleware;
+using SignalRExample.Services;
+
+namespace SignalRExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddMvc();
+            
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<SquaresHub>("/squareshub");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllerRoute(
+                    name: "Default",
+                pattern: "{controller}/{action}",
+                defaults: new { controller = "Square", action = "Index" });
+
+            });
+                        
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
