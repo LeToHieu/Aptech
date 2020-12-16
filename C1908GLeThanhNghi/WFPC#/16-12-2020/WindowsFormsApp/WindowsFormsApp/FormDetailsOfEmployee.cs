01e7bed417af06b97eb1fcd930da4a608b9ca9f1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-12-16 09:42:28 +0700

    update

---
+++ C1908GLeThanhNghi/WFPC#/16-12-2020/WindowsFormsApp/WindowsFormsApp/FormDetailsOfEmployee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class FormDetailsOfEmployee : Form
+    {
+        public FormDetailsOfEmployee()
+        {
+            InitializeComponent();
+            comboBoxCurrentDepartmentName.DataSource = new List<string> {
+                "IT", "Account", "HR"
+            };
+            checkedListBoxDepartmentWorked.Items.Add("IT", CheckState.Checked);
+            checkedListBoxDepartmentWorked.Items.Add("Account", CheckState.Unchecked);
+            checkedListBoxDepartmentWorked.Items.Add("HR", CheckState.Checked);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
