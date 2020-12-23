7b4a8736fe17091630204f88e05db568e13a45e9
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-12-23 13:01:43 +0700

    update

---
+++ C1908GLeThanhNghi/WFPC#/23-12-2020/ExamApp/ExamApp/Database.cs
@@ -0,0 +1,56 @@
+﻿using ExamApp.models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamApp
+{
+    public class Database
+    {
+        private const string CONNECTION_STRING =    @"server=DESKTOP-7PS7HG8\SQLEXPRESS;"+
+                                                    @"database=C1908GLeThanhNghi;" +
+                                                    @"UID=sa;password=123456";
+        private SqlConnection connection;
+        private static Database instance;
+        private Database() { }
+        public static Database GetInstance() {
+            if (instance == null) {
+                instance = new Database();
+            }
+            return instance;
+        }
+        public List<Department> getDepartments()
+        {
+            List<Department> departments = new List<Department>();
+            try
+            {
+                connection = new SqlConnection(CONNECTION_STRING);
+                string query = @"SELECT DepartmentID, DepartmentName FROM Departments";
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                DataSet dataSet = new DataSet();
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                while (sqlDataReader.Read()) {
+                    string departmentName = sqlDataReader[0].ToString();
+                    int departmentId = Convert.ToInt32(sqlDataReader[1]);
+                    Department department = new Department()
+                    {
+                        DepartmentName = departmentName,
+                        DepartmentId = departmentId
+                    };
+                    departments.Add(department);
+                }
+                connection.Close();
+                return departments;
+            }
+            catch (Exception ex) {
+                Console.Error.WriteLine($"Connect DB error : {ex.ToString()}");
+                return departments;
+            }
+        }
+    }
+}
