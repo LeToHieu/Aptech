420a048792d296776f7a47ae412120081da74722
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-02-22 23:49:42 +0700

    update

--- C1908iLeThanhNghi/EAP/22-02-2021/EAP_Music_NguyenVanANew/EAP_Music_NguyenVanANew/Models/Album.cs
+++ C1908iLeThanhNghi/EAP/22-02-2021/EAP_Music_NguyenVanANew/EAP_Music_NguyenVanANew/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,16 @@ namespace EAP_Music_NguyenVanANew.Models
     public class Album
     {
         public int AlbumId { get; set; }
+        [Required]
+        [StringLength(32, MinimumLength =3, ErrorMessage ="Title must be 3 to 32 in length")]
         public string Title { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Artist { get; set; }
+        [Required]
+        [Range(1, 15)]
         public double Price { get; set; }
         public int GenreId { get; set; }
         public virtual Genre Genre { get; set; }
