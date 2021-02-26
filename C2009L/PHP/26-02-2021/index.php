29a9261f99fd39e487dd8e86cd9671fdb7e46ae3
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-02-26 21:26:20 +0700

    update

--- C2009L/PHP/26-02-2021/index.php
+++ C2009L/PHP/26-02-2021/index.php
@@ -39,8 +39,26 @@
         echo "totalSchoolBookPrice = ".$totalSchoolBookPrice."<br>";
         echo "totalReferenceBookPrice = ".$totalReferenceBookPrice."<br>";
         echo "mean = ".$totalReferenceBookPrice / $numberOfReferenceBooks."<br>";
-
+        //server nhan duoc
+        if(isset($_POST["name"])) {
+            echo "<h1> name = ".$_POST["name"]."</h1>";
+            echo "<h1> email = ".$_POST["email"]."</h1>";
+            echo "<h1> age = ".$_POST["age"]."</h1>";
+            //rank 0 => scalar
+            scalar: vo huong
+        }
     ?>
+    <form action='index.php' method="POST">
+    <!-- Gui tu client len server -->
+        <input type="text" placeholder="Enter name" name="name"><br>
+        <input type="text" placeholder="Enter email" name="email"><br>
+        <input type="text" placeholder="Enter age" name="age"><br>
+        <input type="submit" value="Press me">
+        <!--METHOD GET http://localhost:82/index.php?name=hoang&email=abc%40gmail.com&age=18 -->
+        <!--METHOD POST http://localhost:82/index.php -->
+        <!-- https://www.anphatpc.com.vn/ban-phim-co-choi-game_dm1257.html?gclid=CjwKCAiA1eKBBhBZEiwAX3gqlzCz_Un7YOc2lrCbZkM8JJ5MqGYChz6JopVdHCYSlgyrJjeSUNqESxoCvngQAvD_BwE&brand=3&sort=price-desc&filter=1507 -->
+        
+    </form>
 </body>
 </html>
 
