1d1c20c48d37c92c3d2cdda38934d4b9779c57d1
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-06-04 20:48:17 +0700

    update

---
+++ C2009L/ADF1/04-06-2021/src/main/java/models/Student.java
@@ -0,0 +1,36 @@
+package models;
+
+import interfaces.ILearning;
+import interfaces.IPlayable;
+
+public class Student implements IPlayable, ILearning {
+    //Mot class co the thuc thi nhieu interface
+    //implement interface nao thi phai thuc thi cac phuong thuc cua interface do(tru pt static)
+    private String name;
+    private String email;
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public void playAGame(String gameName) {
+        System.out.println("I am playing "+gameName);
+    }
+
+    @Override
+    public void playFoodball() {
+        System.out.println("I am playing foodball");
+    }
+
+    @Override
+    public int borrowSomeBooks() {
+        return 10;
+    }
+
+    @Override
+    public void goToLibrary(String libraryName) {
+        System.out.println("I am going to "+libraryName);
+    }
+}
