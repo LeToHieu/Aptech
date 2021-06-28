a1df67d6dcc8be8843b933cd0e63138edeaff5ab
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-06-28 16:45:42 +0700

    update

---
+++ C1908iLeThanhNghi/EAD-Java/28-06-2021/MyProject/Models/MyDatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace MyProject.Models
+{
+    public class MyDatabaseContext: DbContext
+    {
+        public MyDatabaseContext() : base();        
+    }
+}
