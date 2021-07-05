27d7e05a5c5f243540d8d9d6989afc665b1cfe17
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-07-05 21:12:48 +0700

    update

--- C2009L/ADF2/30-06-2021/src/main/java/views/LoginView.java
+++ C2009L/ADF2/30-06-2021/src/main/java/views/LoginView.java
@@ -15,7 +15,9 @@ public class LoginView implements IView{
     private final JPasswordField textFieldPassword = new JPasswordField(6);
     private final JButton buttonLogin = new JButton("Login");
     private final GridLayout gridLayout = new GridLayout(3,2);
+
     private ProductListView productListView = new ProductListView(375, 250);
+    private SPlineView sPlineView = new SPlineView(375, 250);
 
     public JFrame getMainFrame() {
         return mainFrame;
@@ -50,12 +52,17 @@ public class LoginView implements IView{
             //Nhieu validation ?
             //goi JPA hoac JDBC, goi thang
             boolean loginSuccess = email.equals("hoang@gmail.com") && password.equals("123456");
-            if(!loginSuccess) {
+            if(loginSuccess == false) {
                 Alert.alert("Warning", "Wrong email/pass", JOptionPane.WARNING_MESSAGE);
                 return;
             }
-            this.productListView.reloadData();
-            this.productListView.show();
+            //show do thi
+//            this.productListView.reloadData();
+//            this.productListView.setLoginView(this);
+//            this.hide();
+//            this.productListView.show();
+            this.sPlineView.setupUI(375, 250);
+            this.sPlineView.show();
 
             //chuyen sang man hinh List
             //System.out.println(String.format("Email = %s, password = %s", textFieldEmail.getText(), textFieldPassword.getPassword()));
@@ -70,12 +77,14 @@ public class LoginView implements IView{
     @Override
     public void show() {
         //mot so viec khac
+        System.out.println("kaka");
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         mainFrame.setLocation(dim.width/2-mainFrame.getSize().width/2, dim.height/2-mainFrame.getSize().height/2);
         mainFrame.setVisible(true);
     }
     @Override
     public void hide() {
+        System.out.println("kaka");
         mainFrame.setVisible(false);
     }
 }
--- C2009L/ADF2/30-06-2021/src/main/java/views/ProductListView.java
+++ C2009L/ADF2/30-06-2021/src/main/java/views/ProductListView.java
@@ -5,8 +5,13 @@ import utilities.DateUtility;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.TimerTask;
 
 public class ProductListView implements IView{
     //Khai niem "state" trong Screen
@@ -15,6 +20,29 @@ public class ProductListView implements IView{
     private JFrame mainFrame = new JFrame();
     private int width, height;
     private ArrayList<Person> persons = new ArrayList<>();
+    //reference 1 - 1
+    private LoginView loginView; //mobx trong React, redux(redux saga, redux thunk) trong React
+    //global state = shared state
+    private int countUp = 0;//internal state
+    private javax.swing.Timer timer;
+    private JLabel labelCounterUp = new JLabel("0");
+
+    /*
+    ProductListView chứa thuộc tính persons(mảng các Person Object)
+    khi một trong các person thay đổi giá trị => Giao diện List phải load theo(mà ko phải tắt ProductListView rôi bật lên)
+    => persons gọi là "internal state"
+    */
+    public void setLoginView(LoginView loginView) {
+        this.loginView = loginView;
+    }
+    //reload state phai nam trong setter
+
+    public void setCountUp(int countUp) {
+        this.countUp = countUp;
+        this.labelCounterUp.setText(String.valueOf(this.countUp));
+        //System.out.println("countUp = "+countUp);
+    }
+
     private Object[][] mapPersonsToData() {
         Object[][] result = new String[persons.size()][];
         try {
@@ -30,6 +58,13 @@ public class ProductListView implements IView{
         }
         return result;
     }
+    //reload state => cho vao setter
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+    //
+
     private void setupFakeData() {
         //fake chi de test UI
         //sau nay du lieu phai lay tu DB
@@ -42,7 +77,6 @@ public class ProductListView implements IView{
         this.height = height;
     }
     public void reloadData() {
-        setupFakeData();
         setupUI(width, height);
     }
     //de them Retype password(can public Gridlayout), can sua tieu de(public JFrame)
@@ -57,9 +91,39 @@ public class ProductListView implements IView{
         // Cho phep table sap xep
         table.setAutoCreateRowSorter(true);
         mainFrame.add(scrollPane, BorderLayout.CENTER);
+        mainFrame.add(labelCounterUp);
         mainFrame.setSize(width, height);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLocationRelativeTo(null);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                if (JOptionPane.showConfirmDialog(mainFrame,
+                        "Are you sure you want to close this window?", "Close Window?",
+                        JOptionPane.YES_NO_OPTION,
+                        JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+
+                }
+            }
+        });
+        //setup time interval
+        timer = null;
+        timer = new Timer(1000,new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //countUp ++; //state thay doi, data phai thay doi
+                setCountUp(++countUp); //set state !
+            }
+        });
+
+        java.util.Timer tt = new java.util.Timer(false);
+        tt.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                timer.start();
+            }
+        }, 0);
     }
 
     @Override
