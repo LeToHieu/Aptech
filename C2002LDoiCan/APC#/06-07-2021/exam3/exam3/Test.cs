f189e307aba6fcbcfbd30f6d6c0f89cec8e19aaf
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-07-06 12:58:40 +0700

    update

---
+++ C2002LDoiCan/APC#/06-07-2021/exam3/exam3/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace exam3
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            ShapeCollection shapeCollection = new ShapeCollection();
+            String choice = "";
+            do {
+
+            } while (choice.ToLower() != "quit");
+            Console.WriteLine("Program ended");
+        }
+    }
+}
