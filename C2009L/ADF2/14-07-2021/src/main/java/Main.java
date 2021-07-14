5bdd5bae59b5190b7d58abe9f4393f4ab4e24298
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-07-14 21:20:53 +0700

    update

--- C2009L/ADF2/14-07-2021/src/main/java/Main.java
+++ C2009L/ADF2/14-07-2021/src/main/java/Main.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
+        /*
         System.out.println("Multi thread demo");
         List<Thread> threads = new ArrayList<>();
         int numberOfThreads = 1_000_000_000;
@@ -12,6 +10,11 @@ public class Main {
             threads.add(myThread);
         }
         System.out.println("Number of threads: "+threads.size());
-
+         */
+        EncryptDecrypt encryptDecrypt = new EncryptDecrypt();
+        String encryptedString = encryptDecrypt.encryptData("123456");
+        System.out.println(encryptedString);
+        String decryptedString = encryptDecrypt.decryptData(encryptedString);
+        System.out.println(decryptedString);
     }
 }
