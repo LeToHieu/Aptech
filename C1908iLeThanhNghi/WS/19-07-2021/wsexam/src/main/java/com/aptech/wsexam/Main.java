56e87b7358006050648036516a8f893bdea73222
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-07-19 19:38:30 +0700

    update

--- C1908iLeThanhNghi/WS/19-07-2021/wsexam/src/main/java/com/aptech/wsexam/Main.java
+++ C1908iLeThanhNghi/WS/19-07-2021/wsexam/src/main/java/com/aptech/wsexam/Main.java
@@ -1,5 +1,13 @@
 package com.aptech.wsexam;
 
-public class Main {
+import org.hibernate.cfg.Configuration;
 
+import com.mysql.cj.xdevapi.SessionFactory;
+
+public class Main {
+	public static void main(String [] args) {
+		Configuration configuration = new Configuration().configure("hibernate");
+		//SessionFactory sessionFactory = configuration.buildSessionFactory();
+		
+	}
 }
