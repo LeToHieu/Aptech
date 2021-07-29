985a27f3923ce91050863a7f12043c7d14072974
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-07-29 19:57:08 +0700

    update

---
+++ C2002LDoiCan/WFP-C#/29-07-2021/myapp/myapp/Database.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myapp
+{
+    public class Database
+    {
+        private const String connectionString
+            = @"Server=localhost,1445;Database=testdb;User Id=sa;Password=Abc123456789;";            
+        public bool login(String userName, String password) {
+            bool result = false;
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString)) {
+                String sql = "SELECT * FROM tblStudent WHERE tblStudent.UserNm = @userName AND tblStudent.Password = @password;";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@userName", userName);
+                sqlCommand.Parameters.AddWithValue("@password", password);
+                try
+                {
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("haha");
+                        return true;
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {                    
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+            return result;
+        }
+        public List<Dictionary<String, String>> GetClassStudent() {
+            List<Dictionary<String, String>> result = new List<Dictionary<string, string>>();
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                String sql = @"SELECT tblClass.TenLop, tblStudent.TenSv,tblStudent.UserNm, tblStudent.DiaChi "+ 
+                                @"FROM tblStudent "+
+                                @"INNER JOIN tblClass ON tblStudent.MaLop = tblClass.MaLop;";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);                
+                try
+                {
+                    sqlConnection.Open();
+                    SqlDataReader reader = sqlCommand.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Dictionary<String, String > dictionary = new Dictionary<String, String>();
+                        dictionary.Add("TenLop", reader.GetString("TenLop"));
+                        dictionary.Add("SiSo", reader.GetString("SiSo"));
+                        dictionary.Add("TenSv", reader.GetString("TenSv"));
+                        dictionary.Add("DiaChi", reader.GetString("DiaChi"));
+                        result.Add(dictionary);
+                        Console.WriteLine("haha");                        
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);                    
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ C2002LDoiCan/WFP-C#/29-07-2021/myapp/myapp/Views/MainForm.cs
@@ -0,0 +1,66 @@
+﻿using myapp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myapp
+{
+    public partial class MainForm : Form
+    {
+        public LoginForm LoginForm { get; set; }
+        private void SetupDataGridView() {
+            dataGridView.Columns[0].Name = "TenLop";
+            dataGridView.Columns[1].Name = "TenSv";
+            dataGridView.Columns[2].Name = "UserNm";
+            dataGridView.Columns[3].Name = "DiaChi";
+            dataGridView.SelectionMode =
+            DataGridViewSelectionMode.FullRowSelect;
+
+        }
+        public void fetchDataToGridView(List<Dictionary<String, String>> data) { 
+            dataGridView.Rows.Clear(); 
+            foreach (Dictionary<String, String> dictionary in data) {
+                string[] eachRowData = { 
+                    dictionary["TenLop"], 
+                    dictionary["SiSo"], 
+                    dictionary["TenSv"], 
+                    dictionary["UserNm"],
+                    dictionary["DiaChi"],
+
+                };
+                dataGridView.Rows.Add(eachRowData);
+            }
+        }
+        public MainForm()
+        {
+            InitializeComponent();//nam o partial class khac
+            Database database = new Database(); //must use Singleton Object                                  
+            List<Dictionary<String, String>> data = database.GetClassStudent();
+            SetupDataGridView();
+            fetchDataToGridView(data);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnExportXML_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Export successfully");
+        }
+    }
+}
