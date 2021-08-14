06bce98d4bfe3f1218a4a1166fc4b84da7cd6ae9
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-08-14 19:49:53 +0700

    update

---
+++ C2002LDoiCan/WFP-C#/14-08-2021/dvdproject/dvdproject/dvdproject/DetailCatalogForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dvdproject
+{
+    public partial class DetailCatalogForm : Form
+    {
+        public InsertOrUpdate Status { get; set; }
+        public tblDVDLibrary DVDLibrary { get; set; }
+        public DetailCatalogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
