6a44c3c913c018da613dee24224fbfee69fa035f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-09-20 20:56:59 +0700

    update

--- C2009L/AP/20-09-2021/myapp/myapp/Menu.cs
+++ C2009L/AP/20-09-2021/myapp/myapp/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using myapp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,14 @@ namespace myapp
 {
     public class Menu
     {
+        private List<Person> persons = new List<Person>();//field = variable
+        /*
+        public List<Person> Persons { get => persons; 
+            set { 
+                persons = value;
+            } 
+        }
+        */
         public void ShowMenu() {
             string choice = "";
             while (true)
@@ -22,9 +31,11 @@ namespace myapp
                 if (choice.Equals("1")) 
                 {
                     Console.WriteLine("Input");
+                    InputSomePersons();
                 } else if (choice.Equals("2"))
                 {
                     Console.WriteLine("Sort");
+                    SortAllPersons();
                 }
                 else if (choice.Equals("3"))
                 {
@@ -62,6 +73,34 @@ namespace myapp
                 }
             }
             Console.WriteLine("Program exited");
+        }
+        public void InputSomePersons()
+        {
+            Console.WriteLine("Number of persons: ");
+            int numerOfPersons = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= numerOfPersons; i++)
+            {
+                Console.WriteLine($"Please input Person[{i}]");
+                Person person = new Person();
+                person.Input();
+                persons.Add(person);
+            }
+            this.ShowAllPersons();
+        }
+        public void ShowAllPersons()
+        {
+            foreach(Person person in this.persons)
+            {
+                Console.WriteLine("+------------------------------------------------------------------+");
+                Console.WriteLine("Person Name | Nationality | Birth Year | Net Worth(billion $) |");
+                Console.WriteLine("+ ------------------------------------------------------------------+");
+                Console.WriteLine(person.ToString());
+            }
+        }
+        public void SortAllPersons()
+        {
+            this.persons.Sort((p1, p2) => (int)(p1.NetWorth - p2.NetWorth));
+            ShowAllPersons();
         }
     }
 }
