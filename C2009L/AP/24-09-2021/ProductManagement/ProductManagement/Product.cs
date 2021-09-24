b21066ef9eeec022887700161a158081a0e0dcec
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-09-24 19:32:15 +0700

    update

---
+++ C2009L/AP/24-09-2021/ProductManagement/ProductManagement/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Product
+{
+    //sealed class = cannot be inherited
+    //sealed abstract => NO!
+    public abstract class Product
+    {
+        protected string ProductId {  get; set; }
+        protected string ProductName { get; set; }
+        protected int Year {  get; set; }
+        protected float Price {  get; set; }
+        public abstract void Input();//abstract = has no content
+        public abstract void Display();//abstract = has no content
+        public virtual void DoSomething()
+        {
+            //private virtual => NO!
+        }
+    }
+}
