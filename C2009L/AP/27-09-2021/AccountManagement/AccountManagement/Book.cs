bd3a86f353dd30ea69cc113b02dd46b75820ef34
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-09-27 21:59:44 +0700

    update

--- C2009L/AP/27-09-2021/AccountManagement/AccountManagement/Book.cs
+++ C2009L/AP/27-09-2021/AccountManagement/AccountManagement/Book.cs
@@ -12,12 +12,15 @@ namespace BookManagement
         public DateTime InputDate {  get; set; }
         public double Price { get; set; }
         public int Amount { get; set; }
-        //1 = new, 2 = old => NO !
-        public BookType BookType { get; set; }
-        public void Input()
+        public string Publisher { get; set; }
+        public virtual void Input()
         {
             Console.WriteLine("Enter code: ");
             Code = Console.ReadLine()?? "".ToLower();
+
+            Console.WriteLine("Enter input date(dd/mm/yyyy): ");
+            string stringInputDate = Console.ReadLine() ?? "".ToLower();
+            InputDate = Utility.ToDateTime(stringInputDate);
 
             //input date ?
             Console.WriteLine("Enter price: ");
@@ -25,24 +28,7 @@ namespace BookManagement
 
             Console.WriteLine("Enter amount:");
             Amount = Convert.ToInt32(Console.ReadLine());
-
-
-            /*
-            string stringBookType = "";
-            while(!stringBookType.Equals("old") && !stringBookType.Equals("new"))
-            {
-                Console.WriteLine("Enter bootype(please enter old or new):");
-                stringBookType = Console.ReadLine() ?? "".ToLower();
-                BookType = stringBookType.Equals("old") ? BookType.Old : BookType.New;
-            }
-            */
-            int integerBookType = -1;
-            while (integerBookType != 1 && integerBookType != 2)
-            {
-                Console.WriteLine("Enter bootype(please enter 1(old) or 2(new)):");
-                integerBookType = Convert.ToInt32(Console.ReadLine());
-                BookType = integerBookType == 1 ? BookType.Old : BookType.New;
-            }
+            
         }
     }
 }
--- C2009L/AP/27-09-2021/AccountManagement/AccountManagement/Program.cs
+++ C2009L/AP/27-09-2021/AccountManagement/AccountManagement/Program.cs
@@ -7,6 +7,7 @@ namespace AccountManagement
         public static void Main(string [] args)
         {
             List<Book> books = new List<Book>();
+            /*
             Console.WriteLine("Enter number of books");
             int numberOfBooks = Convert.ToInt32 (Console.ReadLine());
             for(int i = 0; i < numberOfBooks;i++)
@@ -16,7 +17,39 @@ namespace AccountManagement
                 book.Input();
                 books.Add(book);
             }
+            */
             Console.WriteLine("haha");
+            //fake books
+            books = new List<Book>()
+            { 
+                new ReferenceBook(){ 
+                    Code = "c11",
+                    Price = 11,
+                    Amount = 1,
+                    Publisher = "p1",
+                    Tax = 0.2f
+                },
+                new SchoolBook(){
+                    Code = "c22",
+                    Price = 22,
+                    Amount = 2,
+                    Publisher = "p2",
+                    BookType = BookType.New
+                },
+                new ReferenceBook(){
+                    Code = "c33",
+                    Price = 33,
+                    Amount = 3,
+                    Publisher = "p3",
+                    Tax = 0.15f
+                },
+                new SchoolBook(){
+                    Code = "c44",
+                    Price = 44,
+                    Amount = 4,
+                    Publisher = "p4"
+                },
+            };
         }
     }
 }
