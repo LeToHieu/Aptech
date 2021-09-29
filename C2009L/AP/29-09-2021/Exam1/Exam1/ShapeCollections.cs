64366a054d1137cf7553771bb3d37d54ae45cc5e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-09-29 19:36:11 +0700

    update

---
+++ C2009L/AP/29-09-2021/Exam1/Exam1/ShapeCollections.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+    public class ShapeCollections
+    {
+        private List<Shape> shapes= new List<Shape>();
+        public void AddCircle()
+        {
+            try
+            {
+                Console.WriteLine("Enter radius: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Circle circle= new Circle() { 
+                    Radius= radius,
+                };
+                shapes.Add(circle);
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void AddRectangle()
+        {
+            try
+            {
+                Console.WriteLine("Enter width: ");
+                double witdh = Convert.ToDouble(Console.ReadLine());
+                
+                Console.WriteLine("Enter length: ");
+                double length = Convert.ToDouble(Console.ReadLine());
+
+                Rectangle rectangle = new Rectangle()
+                {
+                    Width = witdh,
+                    Length = length,
+                };
+                shapes.Add(rectangle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
