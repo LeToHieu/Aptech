1ad28dd21900cde9cf2681bd9e598824227e913b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-10-01 14:34:51 +0700

    update

---
+++ C2009i/AP/01-10-2021/Exam2/Exam2/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Exam2
+{
+    public class Book : IBook
+    {
+        //field = variables
+        private string title;
+        private string author;
+        private string publisher;
+        private int year;
+        private string isbn;
+        private List<string> chapters;
+        public Book()
+        {
+            chapters = new List<string>();
+        }
+
+
+
+        public string this[int i] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Title {
+            get => title;
+            set {
+                if (value.Length < 6 || value.Length > 40)
+                {
+                    throw new Exception("Title must be 6 and 40 in length");
+                }
+                title = value;
+            }
+        }
+        public string Author { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Publisher { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Year { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ISBN { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ C2009i/AP/01-10-2021/Exam2/Exam2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+namespace Exam2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
