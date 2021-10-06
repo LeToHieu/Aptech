0b3d5bfbd517c16a8f71eb468aef1a5332d12029
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-10-06 14:41:04 +0700

    add copde

---
+++ C2009i/AP/06-10-2021/Exam3/Exam3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Exam3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeManagement employeeManagement = new EmployeeManagement();
+            /*
+            employeeManagement.InputEmployee();
+            employeeManagement.SearchEmployee();
+            employeeManagement.DisplayEmployees();
+            */            
+            employeeManagement.ListEmployee = new List<TemporaryEmp> {  
+                new TemporaryEmp() { 
+                    EmpID = 11,
+                    EmpName = "Nguyen Van A",
+                    DOB = new DateTime(1989,25,12),
+                    Department = "IT",
+                    NumWork = 2,
+                    WorkDay = 10,
+                },
+                new TemporaryEmp() {
+                    EmpID = 11,
+                    EmpName = "Nguyen Van A",
+                    DOB = new DateTime(1989,25,12),
+                    Department = "IT",
+                    NumWork = 2,
+                    WorkDay = 10,
+                }
+            };
+        }
+    }
+}
