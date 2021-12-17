00e20fd2b08a8604f5ed9f7f567f417baae95f3e
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-12-17 08:09:35 +0700

    update

---
+++ C2009i/WFPC#/17-12-2021/WindowsFormsApp1/WindowsFormsApp1/Forms/TravelListForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Models;
+using WindowsFormsApp1.Repositories;
+namespace WindowsFormsApp1.Forms
+{
+    public partial class TravelListForm : Form
+    {
+        private TravelRepository travelRepository = new TravelRepository();
+        public TravelListForm()
+        {
+
+            InitializeComponent();
+
+            List<Travel> travels = travelRepository.GetTravels();
+            //setup ListView
+            listViewTravels.Columns.Add("Name", 100);
+            listViewTravels.Columns.Add("From", 100);
+            listViewTravels.Columns.Add("To", 100);
+            listViewTravels.Columns.Add("Price", 100);
+            listViewTravels.Columns.Add("Start Date", 100);
+
+            //iterate a list and fetch data
+            foreach (Travel travel in travels) {
+                string[] eachRow = { travel.Name, travel.From, travel.To, $"{travel.Price}", $"{travel.StartDate}" };
+                listViewTravels.Items.Add(new ListViewItem(eachRow));
+            }
+            
+        }
+    }
+}
---
+++ C2009i/WFPC#/17-12-2021/WindowsFormsApp1/WindowsFormsApp1/Repositories/TravelRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Models;
+
+namespace WindowsFormsApp1.Repositories
+{
+    public class TravelRepository
+    {
+        public List<Travel> GetTravels() {
+            //get data somewhere....
+            return new List<Travel>
+            {
+                new Travel() { 
+                    TravelId = 1,
+                    Name = "di choi xx11", 
+                    From = "Hanoi",
+                    To = "HCM City",
+                    Price = 1234.55,
+                    StartDate = new DateTime(2021, 12, 30)
+                },
+                new Travel() {
+                    TravelId = 2,
+                    Name = "di choi xx22",
+                    From = "Hanoi",
+                    To = "HCM City",
+                    Price = 4444.55,
+                    StartDate = new DateTime(2021, 12, 30)
+                },
+                new Travel() {
+                    TravelId = 3,
+                    Name = "di choi xx44",
+                    From = "Hanoi",
+                    To = "HCM City",
+                    Price = 2222.55,
+                    StartDate = new DateTime(2021, 12, 30)
+                },
+                new Travel() {
+                    TravelId = 4,
+                    Name = "di choi xx33",
+                    From = "Hanoi",
+                    To = "HCM City",
+                    Price = 1111.55,
+                    StartDate = new DateTime(2021, 12, 30)
+                },
+
+            };
+        }
+    }
+}
