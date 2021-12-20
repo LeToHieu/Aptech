165e33913384ad9f6aa3a6285852f9d639fcda01
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-12-20 19:29:55 +0700

    add code

---
+++ C2009L/MVC/20-12-2021/WAD_C2009L_NguyenVanA/WAD_C2009L_NguyenVanA/Models/Subject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace WAD_C2009L_NguyenVanA.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        public String SubjectName { get; set; }
+        public virtual ICollection<Exam> Exams { get; set; }
+        //constructor
+        public Subject()
+        {
+            this.Exams = new List<Exam>();
+        }
+    }
+}
