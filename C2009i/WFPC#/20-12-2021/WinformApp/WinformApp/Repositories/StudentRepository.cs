ff59e66cd5384b3f0b1046553a282cf744f7b125
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-12-20 16:01:33 +0700

    update

---
+++ C2009i/WFPC#/20-12-2021/WinformApp/WinformApp/Repositories/StudentRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+CREATE TABLE Students(
+    StudentId INT PRIMARY KEY IDENTITY(1, 1),
+    UserName NVARCHAR(100),
+    Password NVARCHAR(50)
+);
+INSERT INTO Students(UserName, Password)
+VALUES('hoangnd', '123456');
+ */
+
+namespace WinformApp.Repositories
+{
+    public class StudentRepository
+    {
+        private Database database = new Database();
+        public Boolean login(string userName, string password) {
+            try
+            {
+                using (SqlConnection sqlConnection = database.GetConnection())
+                {
+                    SqlCommand sqlCommand = new SqlCommand(
+                        @"SELECT * FROM Students WHERE UserName = @userName AND Password = @password",
+                        sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@userName", userName);
+                    sqlCommand.Parameters.AddWithValue("@password", password);
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+                    while (sqlDataReader.Read())
+                    {
+                        return true;
+                    }
+
+                    // Call Close when done reading.
+                    return false;
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error login to your account: "+e.ToString());
+                return false;
+            }
+        }
+    }
+}
