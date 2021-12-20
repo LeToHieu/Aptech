ea4ea5900baab5cf9add9b1254e6e5b25bec580b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2021-12-20 09:55:27 +0700

    add db file

--- C2009i/WFPC#/20-12-2021/WinformApp/WinformApp/SQLQuery.sql
+++ C2009i/WFPC#/20-12-2021/WinformApp/WinformApp/SQLQuery.sql
@@ -11,3 +11,4 @@ INSERT INTO Students(StudentName, UserName, Password, Address) VALUES
 ('Nguyen Van B', 'nguyendana', 'jduisduy483', 'adress aa 3i928u34'),
 ('Nguyen Van C', 'nvb', 'dskjd', 'adress aa 888'),
 ('Nguyen Van D', 'nguyenvanc', 'jdifhjdu', 'adress aa a33');
+SELECT * FROM Students;
