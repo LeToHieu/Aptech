ed2fd729e4cad46406c37d0cf5e5d22617a0e8c3
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-02-12 11:31:10 +0700

    update

--- AptechPTTH/WS/08-02-2022/Database.sql
+++ AptechPTTH/WS/08-02-2022/Database.sql
@@ -6,3 +6,5 @@ CREATE TABLE TblEmployee(
 	PlaceOfWork VARCHAR(30) NOT NULL,
 	PhoneNo VARCHAR(10) NOT NULL
 );
+INSERT INTO TblEmployee(EmployeeNo, EmployeeName, PlaceOfWork, PhoneNo)
+VALUES('222', 'nguyen van xx', 'hcm', '9038998329');
