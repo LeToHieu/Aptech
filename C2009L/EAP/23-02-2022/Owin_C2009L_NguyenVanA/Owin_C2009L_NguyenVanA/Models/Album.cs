78c5f42778266d64cf15b962c8671a2d371d53cd
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-02-23 19:16:55 +0700

    add code

---
+++ C2009L/EAP/23-02-2022/Owin_C2009L_NguyenVanA/Owin_C2009L_NguyenVanA/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Owin_C2009L_NguyenVanA.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ C2009L/EAP/23-02-2022/Owin_C2009L_NguyenVanA/Owin_C2009L_NguyenVanA/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Net.Http;
+
+namespace Owin_C2009L_NguyenVanA
+{
+    internal class Program
+    {
+        private static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            const string SERVER_NAME = "localhost";
+            const string PORT = "9000";
+            string baseAddress = $"http://{SERVER_NAME}:{PORT}/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+
+                string urlGetAllAlbum = $"{baseAddress}api/Album";
+                var response = client.GetAsync(urlGetAllAlbum).Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                
+                //Console.WriteLine($"Server is started at : {PORT}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
