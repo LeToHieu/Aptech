b6b4eb269cfeb1e11d41d75de2b1f671246b3799
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-03-19 11:21:48 +0700

    update

--- AptechPTTH/Android/19-03-2022/app/src/main/java/com/example/callwsapp/activities/FoodListActivity.java
+++ AptechPTTH/Android/19-03-2022/app/src/main/java/com/example/callwsapp/activities/FoodListActivity.java
@@ -2,8 +2,10 @@ package com.example.callwsapp.activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,9 +16,10 @@ import android.widget.Toast;
 
 import com.example.callwsapp.R;
 import com.example.callwsapp.adapters.FoodAdapter;
+import com.example.callwsapp.adapters.MoviesAdapter;
 import com.example.callwsapp.adapters.PhotoAdapter;
-import com.example.callwsapp.models.Category;
 import com.example.callwsapp.models.Food;
+import com.example.callwsapp.models.Movie;
 import com.example.callwsapp.models.Photo;
 import com.example.callwsapp.services.IPhotoServiceResponse;
 import com.example.callwsapp.services.PhotoService;
@@ -24,14 +27,16 @@ import com.example.callwsapp.services.PhotoService;
 import java.util.ArrayList;
 
 public class FoodListActivity extends AppCompatActivity {
+    //Movie
+    private ArrayList<Movie> movies = new ArrayList<>();
+    private MoviesAdapter moviesAdapter;
+    private RecyclerView recyclerViewMovie;
+
+
+
     private RecyclerView recycleViewFoods;
-    private RecyclerView recycleViewCategories;
-    private FoodAdapter categoryAdapter;
     private FoodAdapter foodAdapter;
-
-    private ArrayList<Category> categories = new ArrayList();
     private ArrayList<Food> foods = new ArrayList();
-
     //Horizontal or Vertical ?
     private RecyclerView.LayoutManager layoutManager;
 
@@ -100,43 +105,23 @@ public class FoodListActivity extends AppCompatActivity {
         );
 
     }
-    private void getCategories() {
-        //call api later
-        //mock
-        categories.add(new Category(
-                "Most favorites",
-                "20 items")
-        );
-        categories.add(new Category(
-                "Newest",
-                "11 items")
-        );
-        categories.add(new Category(
-                "somethindddd",
-                "22 items")
-        );
-        categories.add(new Category(
-                "hhahah",
-                "20 items")
-        );
-        categories.add(new Category(
-                "Most favorites",
-                "20 items")
-        );
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_list_activity);
-        //category
-        recycleViewCategories = findViewById(R.id.recycleViewCategories);
+        //Movie
+        recyclerViewMovie = findViewById(R.id.recyclerViewMovies);
 
-        //SQLite: insert/delete, NOT Update
-        //user information
-        //location history
-        //licence agreement => DONOT Call API
+        moviesAdapter = new MoviesAdapter(movies);
+        prepareMovieData();
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerViewMovie.setLayoutManager(mLayoutManager);
+        recyclerViewMovie.setItemAnimator(new DefaultItemAnimator());
 
+        recyclerViewMovie.setAdapter(moviesAdapter);
+
+        //Food
         recycleViewFoods = findViewById(R.id.recycleViewFoods);
         getFoods();
         foodAdapter = new FoodAdapter(this, foods);
@@ -145,6 +130,41 @@ public class FoodListActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(getApplicationContext(),
                 LinearLayoutManager.VERTICAL, false);
         recycleViewFoods.setLayoutManager(layoutManager);
+    }
+    private void prepareMovieData() {
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Cat-MaineCoon-Cookie.png/253px-Cat-MaineCoon-Cookie.png"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Cat-MaineCoon-Cookie.png/253px-Cat-MaineCoon-Cookie.png"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
+        movies.add(new Movie(
+                "Mad Max: Fury Road",
+                "Action & Adventure",
+                "2015", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Maltese_puppy_blue_bow.jpg/200px-Maltese_puppy_blue_bow.jpg"));
 
+        moviesAdapter.notifyDataSetChanged();
     }
 }
