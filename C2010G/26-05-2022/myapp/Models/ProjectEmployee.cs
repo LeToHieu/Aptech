4ce185108a3b1acc6721e746720864162d88be45
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-05-26 10:55:52 +0700

    update

---
+++ C2010G/26-05-2022/myapp/Models/ProjectEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace myapp.Models
+{
+    public class ProjectEmployee
+    {        
+        [Key]
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+        public string Tasks { get; set; }
+        public Employee Employee { get; set;}
+        public Project Project { get; set; }
+    }
+}
