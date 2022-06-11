d39008b57c877c433c7a092871f5133bdfc719ab
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-06-11 21:46:54 +0700

    add code

---
+++ C2009GDoiCan/EAP/11-06-2022/EAP_C2009G_NguyenVanA/EAP_C2009G_NguyenVanA/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EAP_C2009G_NguyenVanA.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int RunningTime { get; set; }
+        public float BoxOffice { get; set; }
+        //foreign key
+        public int GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
+    }
+}
