1703631451e46d4ada08c5c2febddfcd860757bb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-07-18 11:27:43 +0700

    update

---
+++ AptechPTTH/EAD/EADDe04/ejbApp/De07-war/src/java/StudentServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import beans.StudentBeanLocal;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author w11
+ */
+public class StudentServlet extends HttpServlet {
+
+    @EJB
+    private StudentBeanLocal studentBean;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");//setting the content type  
+        PrintWriter printWriter=response.getWriter();//get the stream to write the data  
+
+        //writing html in the stream  
+        printWriter.println("<html><body>");  
+        int x = Integer.parseInt(request.getParameter("x"));
+        int y = Integer.parseInt(request.getParameter("y"));
+        int sum = x + y;
+        printWriter.println(String.format("sum = %d", sum));  
+        printWriter.println("</body></html>");                  
+        printWriter.close();//closing the stream  
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //Insert code => call Enterprise Beans
+        
+    }
+
+  
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
