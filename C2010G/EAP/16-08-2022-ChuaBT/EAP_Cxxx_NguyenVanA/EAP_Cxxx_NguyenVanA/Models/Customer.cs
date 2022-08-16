96bcaa227d4ca2c511b729576605a4bb38f6ef9b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-08-16 11:19:46 +0700

    update

---
+++ C2010G/EAP/16-08-2022-ChuaBT/EAP_Cxxx_NguyenVanA/EAP_Cxxx_NguyenVanA/Models/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EAP_Cxxx_NguyenVanA.Models
+{
+    public class Customer
+    {        
+        public int CustomerId { get; set; }
+        [Required]
+        [StringLength(32, MinimumLength =3)]
+        public string FullName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+
+        public int ClassId { get; set; }
+        public Class Class { get; set; }
+
+    }
+}
