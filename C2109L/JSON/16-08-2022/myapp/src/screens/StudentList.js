da86b0c25e9b03602c1635aafaec01a7ff581366
Nguyen Duc Hoang <sunlight4d@gmail.com>
2022-08-16 21:39:59 +0700

    update

---
+++ C2109L/JSON/16-08-2022/myapp/src/screens/StudentList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button, Stack, Form } from 'react-bootstrap'
+
+/**npm install react-bootstrap bootstrap
+ 
+ */
+function StudentList() {
+    return <div>
+        <h1>This is student List</h1>        
+    </div>
+}
+//make public
+export default StudentList
